f6bd093d5db68d44d9089ed08ea69e8d926b4bf3
grezisek1 <grezisek@gmail.com>
2024-08-03 08:06:52 +0200

    add account login and register, add private page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function Uplouder() {
   const supabase = createClient();
-  const { data } = await supabase.from("test").select();
-
-  return <pre>{JSON.stringify(data, null, 2)}</pre>
+  const { data: { user } } = await supabase.auth.getUser();
+  if (user === null) {
+    return "error unreachable";
+  }
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
